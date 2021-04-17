30d1ead362919ffeb36fcc83d284be1d8f8f370d
Lima <andrews.jose.c.lima@accenture.com>
2021-04-16 22:18:50 -0300

    update: added the search function + mocked json data

---
+++ data/data.js
@@ -0,0 +1,198 @@
+const data = [
+	{
+		"index": 1,
+		"nome": "coentro",
+		"cientifico": "coriandrum sativium",
+		"img": "https://arcomixstr.blob.core.windows.net/product/5478-coentro-und-m.jpg",
+		"plantio": "Escolha um vaso com no Mín 15cm de diâmetro e 20cm de profundidade, encha com substrato e ponha a semente a 2cm do solo, mantenha a terra umida mas sem encharcar, deixe o vaso em local iluminado.",
+		"sol": "4",
+		"agua": {
+			"QTDvezes": 1,
+			"aCada": 3
+		},
+		"epoca": "primavera",
+		"ciclo": {
+			"germinacao": 7,
+			"crescimento": 20,
+			"produção": 30,
+			"colheita": 60
+		}
+	},
+
+	{
+		"index": 2,
+		"nome": "tomate",
+		"cientifico": "solanum lycopersicum",
+		"img": "https://www.ecowebdesign.com.br/painel/img/uploads/1587879000_313.png",
+		"plantio": "Escolha um vaso de tamanho médio, encha com uma mistura de terra, areia e composto orgânico em partes iguais, regando apenas quando notar que a superficie está seca, se as folhas começarem a enrolar diminua as regas",
+		"sol":"4",
+		"agua": {
+			"QTDvezes": 1,
+			"aCada": 2
+		},
+		"epoca": "verão",
+		"ciclo": {
+			 "germinação": 10,
+			 "crescimento": 20,
+			 "produção": 60,
+			 "colheita": 90 
+		}
+	},
+
+	{
+		"index": 3,
+		"nome": "alho-poró",
+		"cientifico": "allium porrum",
+		"img": "https://sj-img.azureedge.net/product/8881-alho-poro-un-m.jpg",
+		"plantio": "Tenha em mãos um vaso de no mínimo 25cm de profundidade, ponha a semente a 1cm da terra com substrato e composto orgânico, mantenha o solo sempre umido, a planta não necessita de podagem.",
+		"sol":"2",
+		"agua": 4,
+		"epoca": "outono",
+		"ciclo": {
+			 "germinação": 7,
+			 "crescimento": 30,
+			 "produção": 60,
+			 "colheita": 90
+		}
+	},
+
+	{
+		"index": 4,
+		"nome": "alface",
+		"cientifico": "lactuca sativa",
+		"img": "https://sj-img.azureedge.net/product/4075-alface-lisa-un-m.jpg",
+		"plantio": "utilize uma bandeja com 10cm de altura, forre a bandeja com cascalhos, prepare o solo com terra e composto orgânico, ponha a  semente a 1cm da terra, regue pulverizando para nao afundar a semente, regue sempre que a terra estiver seca.",
+		"sol":"5",
+		"agua": {
+			"QTDvezes": 1,
+			"aCada": 2
+		},
+		"epoca": "primavera",
+		"ciclo": {
+			 "germinação": 7,
+			 "crescimento": 20,
+			 "produção": 45,
+			 "colheita": 90
+		}
+	},
+
+	{
+		"index": 5,
+		"nome": "couve",
+		"cientifico": "brassica oleracea",
+		"img": "https://obasuperatacado-img.azureedge.net/product/5785-couve-folha-un-m.jpg",
+		"plantio": "utilize um vaso com no minimo 25cm de diâmetro, semeando a 1cm de profundidade da terra, o solo deve se manter sempre úmido mas sem que fique encharcado, cultive em luz direta provendo sombra nas horas mais quentes.",
+		"sol":"6",
+		"agua": {
+			"QTDvezes": 1,
+			"aCada": 1
+		},
+		"epoca": "primavera",
+		"ciclo": {
+			 "germinação": 7,
+			 "crescimento": 30,
+			 "produção": 60,
+			 "colheita": 90
+		}
+	},
+	{
+		"index": 6,
+		"nome": "cebolinha",
+		"cientifico": "Allium schoenoprasum",
+		"img": "https://redecompras-img.azureedge.net/product/15895-cebolinha-und-m.jpg",
+		"plantio": "opte vasos com profundidade e diâmetro mínimo de 15 cm, semeando com 2cm de pronfundidade pois parte do caule fica enterrado, o solo deve ser rico em nutrientes, mantenha uma rega frequente para não secar o solo.",
+		"sol":"6",
+		"agua": {
+			"QTDvezes": 2,
+			"aCada": 1
+		},
+		"epoca": "outono",
+		"ciclo": {
+			"germinação": 7,
+			"crescimento": 20,
+			"produção": 50,
+			 "colheita": 80
+		}
+	},
+
+	{
+		"index": 7,
+		"nome": "manjericão",
+		"cientifico": "Ocimum basilicum",
+		"img": "https://obasuperatacado-img.azureedge.net/product/5792-manjericao-un-m.jpg",
+		"plantio": "em um vaso com no mínimo 15cm de diâmetro ponha a semente a 1cm do solo, regando incialmente com pulverizador para evitar o excesso de água, cultive em lugares mais quentes, o manjericão não suporta frio e ventos fortes.",
+		"sol":"6",
+		"agua": {
+			"QTDvezes": 1,
+			"aCada": 3
+		},
+		"epoca": "primavera",
+		"ciclo": {
+			 "germinação": 7,
+			 "crescimento": 30,
+			 "produção": 60,
+			 "colheita": 90
+		}
+	},
+
+	{
+		"index": 8,
+		"nome": "Orégano",
+		"cientifico": "Origanum vulgare",
+		"img": "https://www.emporiodasfrutasam.com.br/data/produto/m/465-657.jpg",
+		"plantio": "Em um vaso de proporção pequena ponha a semente sobre a terra sem cobrir e regue com um pulverizador sem deixar encharcar, regue apenas em períodos muito seco,  orégano gosta de bastante sol, deixar sem sobra nas horas mais quentes.",
+		"sol":"4",
+		"agua": {
+			"QTDvezes": 1,
+			"aCada": 2
+		},
+		"epoca": "primavera",
+		"ciclo": {
+			 "germinação": 5 ,
+			 "crescimento": 30 ,
+			 "produção": 60 ,
+			 "colheita": 90 
+		}
+	},
+
+	{
+		"index": 9,
+		"nome": "alecrim",
+		"cientifico": "Salvia rosmarinus",
+		"img": "https://images.rappi.com.br/products/2093844280-1578051050943.jpg?d=200x200",
+		"plantio": "Ponha a semente a 1cm do solo em um vaso com 20cm de diâmetro, ultilizando solo comum pois a planta se adapta a todos os tipos de solo, regue 1 vez a cada 2 dias, deixe o vaso em local arejado e com boa luminosidade.",
+		"sol":"3",
+		"agua": {
+			"QTDvezes": 1,
+			"aCada": 2
+		},
+		"epoca": "primavera",
+		"ciclo": {
+			 "germinação": 28,
+			 "crescimento": 60,
+			 "produção": 120,
+			 "colheita": 180
+		}
+	},
+
+	{
+		"index": 10,
+		"nome": "cebola",
+		"cientifico": "Allium cepa",
+		"img": "https://obasuperatacado-img.azureedge.net/product/5307-hort-cebola-kg-m.jpg",
+		"plantio": "o plantio de cebola necessita de um vaso de médio para grande, em uma terra com textura média para não acumular água, deixe o solo úmido regando a cada 2 dias, a cebola gosta de luminosidade mas não precisa de muita.",
+		"sol":"várias",
+		"agua": {
+			"QTDvezes": 1,
+			"aCada": 2
+		},
+		"epoca": "outono",
+		"ciclo": {
+			 "germinação": 10 ,
+			 "crescimento": 30 ,
+			 "produção": 60 ,
+			 "colheita": 90 
+		}
+	}
+]
+ export default data; 
--- pages/pesquisa/pesquisa.js
+++ pages/pesquisa/pesquisa.js
@@ -1,12 +1,74 @@
-const input = document.getElementById("pesquisa")
-const card = document.querySelector(".card")
-const title = document.querySelector("#inputTitle")
-input.addEventListener("input", (evt) => {
-   if (evt.target.value == "orégano"){
-    card.className = "card"
-    title.innerHTML = "Ótima escoha"
-   }  else{
-       card.className = "card hide"
-       title.innerHTML = "O que vamos plantar ?"
-    } 
-})
+import data from "../../data/data.js"; //importo o arquivo data.js onde contém o json
+console.log(data); // serve para visualmente verificar se data está retornando o json corretamente
+const cardContainer = document.querySelector(".cards"); //cardContainer armazena o elemento com class "cards" que é onde um card fica dentro
+//um card é o elemento/container/quadrado branco onde aparecem os dados da planta
+
+//aqui estou dizendo para adicionar html dentro deste cardContainer,  esse html é o card onde contém informações da planta
+cardContainer.innerHTML = `
+    ${data.map((planta) => {
+        //aqui é mapeado o json. ele vai repetir este html para cada objeto(plantaa) que existir no json
+        // o map instância o objeto json atual numa variável que tem o nome determinado ali em cima em parênteses
+        // agora você pode chamar a planta.img, planta.nome planta.plantio etc... 
+			return `
+        <div class="card hide">  
+         <header>
+             <div class="circle">
+             <img src="${planta.img}" alt="${planta.nome}"/>
+             </div>
+             <h2 class="title">${planta.nome}</h2>
+         </header>
+
+         <section class="info">
+             <h3 class="title">Melhor estação para cultivo:</h3>
+             <p class="content">${planta.epoca}</p>
+             <h3 class="title">Forma de cultivo:</h3>
+             <p class="content">${planta.plantio}</p>
+         </section>
+	 </div>
+        `;
+		})
+		.join("")} 
+`;
+// este .join("") serve para substituir virgulas que são imprimidas ao fim de cada json por uma string vazia ""
+
+/* O window.onload = () => { seu código aqui } serve para que o código dentro seja rodado apenas depois
+    que o conteúdo da página for carregado ( onLoad = ao carregar), se não for feito dessa forma, você não consegue acessar o 
+    elemento .card acima, pois o DOM iria acusar que esse elemento com classe .card não existe no documento. 
+*/
+window.onload = () => {
+	const cards = cardContainer.querySelectorAll(".card"); // A variável card agora é um array que contém todos os elementos com a classe "card"
+	const input = document.getElementById("pesquisa");	// A variável input armazena o elemento input que contém o ID pesquisa
+	input.addEventListener("input", (evt) => { //é adicionado um escutador de eventos do tipo input, que é disparado cada vez que é digitado no campo da variavel input
+		var value = evt.target.value; //armazena o valor atual do input no value
+        const title = document.querySelector("#inputTitle"); // A variável title agora armazena o elemento com ID inputTitle
+        for (let i = 0; i < cards.length; i++) { //O for é um laço de repetição que vai repetir o código abaixo um número de vezes igual ao comprimento do array card
+
+            const nomeDaPLanta = cards[i].querySelector(".title").innerHTML //pega o nome da planta da posição atual do array
+            if(value == nomeDaPLanta) { //se o valor do input da busca for igual ao nome da planta da posição atual ele tira a classe hide deste card, que faz ele ser mostrado na tela
+                cards[i].className = "card";
+                title.innerHTML = "Ótima escolha" // muda o título do para Ótima escolha
+                break; // o break; serve para quebrar a repetição do for. ele diz pro for que se chegar aqui, ele não deve mais repetir este código
+                
+            } else{ // se o if for falso ele adiciona a classe hide  ao elemento atual
+                cards[i].className = "card hide";
+			    title.innerHTML = "O que vamos plantar ?";
+            }
+        }
+
+        //  Poderia ser feito com um forEach ou map também, mas esses métodos não permitem o uso do break, então se houvesse um json com 100 posições
+        //  mesmo que o resultado fosse encontrado na posição 3 ele iria continuar repetindo o código mais 97 vezes
+        //  resultando assim numa queda de performance do nosso site
+
+        // card.forEach( planta => {
+        //     const nomeDaPLanta = planta.querySelector(".title").innerHTML
+        //     if(value == nomeDaPLanta) {
+        //         planta.className = "card";
+        //         return title.innerHTML = "Ótima escolha"
+                
+        //     } else{
+        //         planta.className = "card hide";
+		// 	    title.innerHTML = "O que vamos plantar ?";
+        //     }
+        // })			
+	});
+};
