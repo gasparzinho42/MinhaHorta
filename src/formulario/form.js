8c19fa6281ba4720316d83d88daecaa5288e8e00
Andrews Lima <andrews.jose.c.lima@accenture.com>
2021-06-14 20:22:34 -0300

    fix: fixed bug on CEP

--- src/formulario/form.js
+++ src/formulario/form.js
@@ -6,7 +6,6 @@ var rua = document.getElementById("rua");
 var bairro = document.getElementById("bairro");
 var cidade = document.getElementById("cidade");
 
-
 fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
 	.then((estado) => estado.json())
 	.then((estado) => {
@@ -29,18 +28,25 @@ telefone.addEventListener("input", (evt) => {
 
 /*Logica de CEP */
 cep.addEventListener("input", (evt) => {
-    let value = evt.target.value;
-    let formatedValue = value.replace(/\D/g,"")
-    formatedValue = formatedValue.replace(/(\d{5})(\d{3})/g,"$1-$2");
-    evt.target.value = formatedValue;
+	let value = evt.target.value;
+	let formatedValue = value.replace(/\D/g, "");
+	formatedValue = formatedValue.replace(/(\d{5})(\d{3})/g, "$1-$2");
+	evt.target.value = formatedValue;
 	if (value.length === 8) {
 		fetch(`https://brasilapi.com.br/api/cep/v1/${value}`)
 			.then((response) => response.json())
 			.then((response) => {
-				bairro.value = response.neighborhood;
-				rua.value = response.street;
-				uf.value = response.state;
-				cidade.value = response.city;
+				if (!response.errors) {
+					bairro.value = response.neighborhood;
+					rua.value = response.street;
+					uf.value = response.state;
+					cidade.value = response.city;
+				} else {
+					bairro.value = "";
+					rua.value = "";
+					uf.value = "";
+					cidade.value = "";
+				}
 			});
 	}
 });
